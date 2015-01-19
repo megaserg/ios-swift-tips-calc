b03d140862c0c940d0e877e927948251991e8d89
Sergey Serebryakov <sserebryakov@twitter.com>
2015-01-19 01:21:43 -0500

    Add animation and app icon

--- README.md
+++ README.md
@@ -2,18 +2,16 @@
 
 This is an iOS-Swift application for calculation of the tips given the bill amount.
 
-Time spent: 1+ hours spent in total
+Time spent: about 6 hours spent in total
 
 Completed user stories:
 
  * [x] Basic: User can input a bill amount, tap anywhere to hide the decimal keyboard, then choose the tip percentage to see the resulting tip and total amount.
- * [x] Required: User can tap Settings and set default tip percentage and background color. The background color setting is applied immediately and persists after relaunch. The default tip percentage setting is applied after relaunch.
- * [x] Required: ...
- * [x] Optional: ...
+ * [x] Settings: User can tap Settings and set default tip percentage and background color. The background color setting is applied immediately and persists after relaunch. The default tip percentage setting is applied after relaunch.
+ * [x] Optional: The bill amount and percentage entered by user persist after relaunch for 15 seconds.
+ * [x] Optional: The informational view fades in and out depending on whether any amount is entered.
+ * [x] Optional: The app has an icon!
  
-Notes:
-
-Spent some time making the UI work across multiple phone resolutions by playing around with the RelativeLayout.
 
 Walkthrough of all user stories:
 
--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -11,19 +11,42 @@ import UIKit
 class ViewController: UIViewController {
     
     var defaults = NSUserDefaults.standardUserDefaults()
+    
+    let DEFAULTS_BILL_AMOUNT_KEY = "last_bill_amount"
+    let DEFAULTS_LAST_TIP_KEY = "last_tip_index"
+    let DEFAULTS_BILL_DATE_KEY = "bill_amount_changed_date"
 
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var hidableView: UIView!
+    
+    @IBOutlet weak var billAmountLabel: UILabel!
+    @IBOutlet weak var tipCaptionLabel: UILabel!
+    @IBOutlet weak var totalCaptionLabel: UILabel!
+    @IBOutlet weak var dividerView: UIView!
+    
+    var viewIsHidden = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        tipLabel.text = "$0.00"
-        totalLabel.text = "$0.00"
-        tipControl.selectedSegmentIndex = defaults.integerForKey(DEFAULTS_TIP_KEY)
+        let lastEditDate = defaults.objectForKey(DEFAULTS_BILL_DATE_KEY) as NSDate?
+        let currentDate = NSDate()
+        
+        if (lastEditDate != nil && currentDate.timeIntervalSinceDate(lastEditDate!) < 15) {
+            let billAmountText = defaults.objectForKey(DEFAULTS_BILL_AMOUNT_KEY) as NSString
+            billField.text = billAmountText
+            tipControl.selectedSegmentIndex = defaults.integerForKey(DEFAULTS_LAST_TIP_KEY)
+            updateTipLabels(billAmountText)
+        } else {
+            billField.text = ""
+            tipControl.selectedSegmentIndex = defaults.integerForKey(DEFAULTS_TIP_KEY)
+            tipLabel.text = "$0.00"
+            totalLabel.text = "$0.00"
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,15 +55,52 @@ class ViewController: UIViewController {
     }
 
     @IBAction func onEditingChanged(sender: AnyObject) {
-        var tipPercentages = [0.18, 0.20, 0.22]
-        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
-        
-        var billAmount = (billField.text as NSString).doubleValue
-        var tip = billAmount * tipPercentage
-        var total = billAmount + tip
-        
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
+        let billAmountText = billField.text as NSString
+        defaults.setObject(billAmountText, forKey: DEFAULTS_BILL_AMOUNT_KEY)
+        defaults.setObject(tipControl.selectedSegmentIndex, forKey: DEFAULTS_LAST_TIP_KEY)
+        defaults.setObject(NSDate(), forKey: DEFAULTS_BILL_DATE_KEY)
+        defaults.synchronize()
+
+        updateTipLabels(billAmountText)
+    }
+    
+    func updateTipLabels(billAmount: NSString) {
+        if billAmount != "" {
+            let tipPercentages = [0.18, 0.20, 0.22]
+            let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+            
+            let value = billAmount.doubleValue
+            let tip = value * tipPercentage
+            let total = value + tip
+            
+            tipLabel.text = String(format: "$%.2f", tip)
+            totalLabel.text = String(format: "$%.2f", total)
+            
+            if viewIsHidden {
+                showHidableView()
+                viewIsHidden = false
+            }
+        } else {
+            if !viewIsHidden {
+                hideHidableView()
+                viewIsHidden = true
+            }
+        }
+    }
+    
+    func showHidableView() {
+        animateView(0, finishVal: 1)
+    }
+    
+    func hideHidableView() {
+        animateView(1, finishVal: 0)
+    }
+    
+    func animateView(startVal: CGFloat, finishVal: CGFloat) {
+        self.hidableView.alpha = startVal
+        UIView.animateWithDuration(0.4, animations: {
+            self.hidableView.alpha = finishVal
+        })
     }
     
     @IBAction func onTap(sender: AnyObject) {
@@ -49,25 +109,29 @@ class ViewController: UIViewController {
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        var bgColors = [UIColor.whiteColor(), UIColor.grayColor(), UIColor.greenColor()]
-        var chosenColor = defaults.integerForKey(DEFAULTS_COLOR_KEY)
-        view.backgroundColor = bgColors[chosenColor]
+        
+        let bgColors = [UIColor.whiteColor(), UIColor.darkGrayColor(), UIColor.greenColor()]
+        let fgColors = [UIColor.blackColor(), UIColor.whiteColor(), UIColor.orangeColor()]
+        let ttColors = [view.tintColor, UIColor.lightGrayColor(), UIColor.redColor()]
+        
+        let colorIndex = defaults.integerForKey(DEFAULTS_COLOR_KEY)
+        let bgColor = bgColors[colorIndex]
+        let fgColor = fgColors[colorIndex]
+        let ttColor = ttColors[colorIndex]
+        
+        view.backgroundColor = bgColor
+        hidableView.backgroundColor = bgColor
+        
+        billAmountLabel.textColor = fgColor
+        tipCaptionLabel.textColor = fgColor
+        totalCaptionLabel.textColor = fgColor
+        
+        tipLabel.textColor = fgColor
+        totalLabel.textColor = fgColor
+        
+        dividerView.backgroundColor = fgColor
+        
+        tipControl.tintColor = ttColor
     }
-    
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
-        println("view did appear")
-    }
-    
-    override func viewWillDisappear(animated: Bool) {
-        super.viewWillDisappear(animated)
-        println("view will disappear")
-    }
-    
-    override func viewDidDisappear(animated: Bool) {
-        super.viewDidDisappear(animated)
-        println("view did disappear")
-    }
-
 }
 
